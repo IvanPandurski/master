fa47efe082d43ef84524759d361442ef4e50a16d
Ivan Pandurski <ivan.pandurski@abv.bg>
2020-11-05 18:29:05 +0200

    add new open gl windows project

--- data_struct/main.cpp
+++ data_struct/main.cpp
@@ -5,8 +5,8 @@
 
 //#include "InfixToPostfix.h"
 //#include "binarytreegenerator.h"
-//#include "SDL.h"
-//#include "GL/gl.h"
+#include "SDL.h"
+#include "GL/gl.h"
 #include "sorting.h"
 #include "hashing.h"
 #include "graph.h"
@@ -124,36 +124,36 @@ int main(int argc, char *argv[])
 
 
 
-//    SDL_Window *window;                    // Declare a pointer
+    SDL_Window *window;                    // Declare a pointer
 
-//        SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+        SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
 
-//        // Create an application window with the following settings:
-//        window = SDL_CreateWindow(
-//            "An SDL2 window",                  // window title
-//            SDL_WINDOWPOS_UNDEFINED,           // initial x position
-//            SDL_WINDOWPOS_UNDEFINED,           // initial y position
-//            640,                               // width, in pixels
-//            480,                               // height, in pixels
-//            SDL_WINDOW_OPENGL                  // flags - see below
-//        );
+        // Create an application window with the following settings:
+        window = SDL_CreateWindow(
+            "An SDL2 window",                  // window title
+            SDL_WINDOWPOS_UNDEFINED,           // initial x position
+            SDL_WINDOWPOS_UNDEFINED,           // initial y position
+            640,                               // width, in pixels
+            480,                               // height, in pixels
+            SDL_WINDOW_OPENGL                  // flags - see below
+        );
 
-//        // Check that the window was successfully created
-//        if (window == NULL) {
-//            // In the case that the window could not be made...
-//            printf("Could not create window: %s\n", SDL_GetError());
-//            return 1;
-//        }
+        // Check that the window was successfully created
+        if (window == NULL) {
+            // In the case that the window could not be made...
+            printf("Could not create window: %s\n", SDL_GetError());
+            return 1;
+        }
 
-//        // The window is open: could enter program loop here (see SDL_PollEvent())
+        // The window is open: could enter program loop here (see SDL_PollEvent())
 
-//        SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+        SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
 
-//        // Close and destroy the window
-//        SDL_DestroyWindow(window);
+        // Close and destroy the window
+        SDL_DestroyWindow(window);
 
-//        // Clean up
-//        SDL_Quit();
+        // Clean up
+        SDL_Quit();
 
 
     return 0;
---
+++ openGL_win/build/CMakeFiles/openGL_win.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openGL_win.dir/main.cpp.obj"
+  "libopenGL_win.dll.a"
+  "openGL_win.exe"
+  "openGL_win.exe.manifest"
+  "openGL_win.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openGL_win.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
